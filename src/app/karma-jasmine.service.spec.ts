aadcb3a8f5140ed5d54c6ac313d996246d273740
tomfacal <88186642+tomfacal@users.noreply.github.com>
2023-06-27 08:34:56 +0200

    fist commit

---
+++ src/app/karma-jasmine.service.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed } from '@angular/core/testing';
+import { KarmaJasmineService } from './karma-jasmine.service';
+
+describe('KarmaJasmineService', () => {
+  let service: KarmaJasmineService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.get(KarmaJasmineService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('debería sumar dos números correctamente', () => {
+    const resultado = service.sumar(2, 3);
+    expect(resultado).toBe(5);
+  });
+
+  it('debería sumar números negativos correctamente', () => {
+    const resultado = service.sumar(-5, -3);
+    expect(resultado).toBe(-8);
+  });
+
+  it('debería sumar un número positivo y un número negativo correctamente', () => {
+    const resultado = service.sumar(10, -5);
+    expect(resultado).toBe(5);
+  });
+
+});
---
+++ src/app/karma-jasmine.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class KarmaJasmineService {
+
+  constructor() { }
+
+  sumar(a: number, b: number): number {
+    return a + b;
+  }
+
+
+}
